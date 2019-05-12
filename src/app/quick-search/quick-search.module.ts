36a940beff000a370ced9216103b916b20f72f9e
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-05-12 20:56:12 +0700

    Init pembuatan awal

---
+++ src/app/quick-search/quick-search.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { QuickSearchRoutingModule } from './quick-search-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    QuickSearchRoutingModule
+  ]
+})
+export class QuickSearchModule { }
